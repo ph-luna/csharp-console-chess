746af28a18c252ef008480012b74e21c554babb8
ph luna <ph.luna.vieira@gmail.com>
2023-11-20 12:18:57 -0300

    feat: visual improviment

--- Application/Screen.cs
+++ Application/Screen.cs
@@ -1,26 +1,56 @@
 
 using board;
+using Pastel;
 
 class Screen
 {
-  public static void printBoard(Board board)
+  private const string WhiteColor = "#FFFFFF";
+  private const string BlackColor = "#000000";
+  private const string YellowColor = "#ffe599";
+  private const string GreyColor = "#5a5a5a";
+  public static void PrintPiece(Piece piece, bool isOdd)
+  {
+    var background = BlackColor;
+    var pieceColor = YellowColor;
+    if (isOdd)
+    {
+      background = GreyColor;
+    }
+    if (piece.Color == Color.White)
+    {
+      pieceColor = WhiteColor;
+    }
+
+    Console.Write(piece.ToString().Pastel(pieceColor).PastelBg(background) + " ".PastelBg(background));
+  }
+
+  public static void PrintBoard(Board board)
   {
     for (int l = 0; l < board.Lines; l++)
     {
+      Console.Write(8 - l + " ");
       for (int c = 0; c < board.Columns; c++)
       {
+        bool isOdd = (l + c) % 2 != 0;
         var piece = board.GetPieceInPosition(l, c);
         if (piece is null)
         {
-          bool isOdd = (l + c) % 2 != 0;
-          Console.Write(isOdd ? "□ " : "■ ");
+          if (isOdd)
+          {
+            Console.Write("  ".PastelBg(GreyColor));
+          }
+          else
+          {
+            Console.Write("  ");
+          }
         }
         else
         {
-          Console.Write(piece + " ");
+          PrintPiece(piece, isOdd);
         }
       }
       Console.WriteLine();
     }
+    Console.WriteLine("  a b c d e f g h");
   }
 }
